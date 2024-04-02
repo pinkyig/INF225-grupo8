54bfaad2e5b276cd6364871c8d5179903455c4af
crisguerrero02 <144069721+crisguerrero02@users.noreply.github.com>
2024-04-01 22:40:50 -0300

    Delete proyecto/client/public directory

--- proyecto/client/public/src/components/CreateHora.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class CreateHora extends Component {
-    render() {
-        return (
-            <div>Agendamiento de horas</div>
-        )
-    }
-}
--- proyecto/client/public/src/components/VerPerfil.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class VerPerfil extends Component {
-    render() {
-        return (
-            <div>Ver Perfil</div>
-        )
-    }
-}
