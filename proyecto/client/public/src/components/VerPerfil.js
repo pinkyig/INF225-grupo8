348d8100dba543f123f01eb651393fd95430eb10
crisguerrero02 <144069721+crisguerrero02@users.noreply.github.com>
2024-04-01 22:36:05 -0300

    Create VerPerfil.js

---
+++ proyecto/client/public/src/components/VerPerfil.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class VerPerfil extends Component {
+    render() {
+        return (
+            <div>Ver Perfil</div>
+        )
+    }
+}
