8f9351eec511a78bb850be9d8f75d91b9fac3670
crisguerrero02 <144069721+crisguerrero02@users.noreply.github.com>
2024-04-01 22:23:49 -0300

    Update app.js

--- proyecto/server/app.js
+++ proyecto/server/app.js
@@ -1,51 +1,26 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const morgan = require("morgan");
-
 const db = require("./Database");
-
-const ObtenerDatos = async () => {
-    try {
-        const resultado = await horarios.find({
-            especialidad:"Imagenologia"
-        });
-
-      
-        const estructura = resultado.map(item => ({
-            Rut: item.especialidad,
-            Examen: item.tipo_examen,
-            Hora: item.hora
-        }));
-
-        estructura.forEach(item => {
-            console.log(item.Rut);
-            console.log(item.Examen);
-            console.log(item.Hora);
-
-        });
-    } catch (error) {
-        console.error("Error al obtener datos:", error);
-    }
-};
-
-ObtenerDatos()
-
-
 const app = express();
 
-app.set("port", process.env.PORT || 500);
 
+//
+app.set("port", process.env.PORT || 5000);
+
+//Middlewares
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
 
-db()
+//Routes
+app.use("/api/ficha", require("./Routes/ficha"));
+app.use("/api/horas", require("./Routes/hora"));
 
-app.listen(app.get("port") , ()=>{
-    console.log('Servidor esta corriendo en el puerto: ${app.get("port")}');
+db();
+
+app.listen(app.get("port"), () => {
+    console.log(`Servidor esta corriendo en el puerto: ${app.get("port")}`);
 });
-
 
 module.exports = app;
